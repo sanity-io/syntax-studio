3d09b263cccacca3aa83cc70c07b2e384025c4a2
Knut Melvær <knut.melvaer@gmail.com>
2018-09-03 23:23:09 +0200

    add webamp

--- plugins/syntaxfm-logo/ImgLogo.js
+++ plugins/syntaxfm-logo/ImgLogo.js
@@ -1,11 +1,34 @@
-import React from 'react'
-import styles from './ImgLogo.css'
+import React, { Component, Fragment } from "react";
+import Winamp from "./winamp/Winamp";
+
+import styles from "./ImgLogo.css";
 // While we recommend SVGs, an alternative is to use a regular image (png, jpg)
 // Notice how we're "importing" the path from a local image - when building the
 // studio, this image will automatically be part of the output bundle, with a
 // hash that will make it easy to invalidate on changes.
-import syntaxfmLogo from './logo.png'
+import syntaxfmLogo from "./logo.png";
 
-const ImgLogo = () => <img className={styles.logo} src={syntaxfmLogo} />
+class ImgLogo extends Component {
+  state = {
+    playerActivated: false
+  };
 
-export default ImgLogo
+  togglePlayer = () =>
+    this.setState({ playerActivated: !this.state.playerActivated });
+
+  render() {
+    const { playerActivated } = this.state || {};
+    return (
+      <Fragment>
+        <img
+          onClick={this.togglePlayer}
+          className={styles.logo}
+          src={syntaxfmLogo}
+        />
+        {playerActivated && <Winamp />}
+      </Fragment>
+    );
+  }
+}
+
+export default ImgLogo;
---
+++ plugins/syntaxfm-logo/winamp/Winamp.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import client from "part:@sanity/base/client";
+import Webamp from "webamp";
+export default class Winamp extends Component {
+  componentDidMount() {
+    // Or, if you installed via a script tag, `Winamp` is available on the global `window`:
+    // const Winamp = window.Webamp;
+    // Check if Winamp is supported in this browser
+    this.mountWebamp();
+  }
+  mountWebamp = () => {
+    if (!Webamp.browserIsSupported()) {
+      alert("Oh no! Webamp does not work!");
+      throw new Error("What's the point of anything?");
+    }
+    return client
+      .fetch(
+        `*[_type == "episode"]{file{asset->}, title}|order(schedule.publish desc)`
+      )
+      .then(episodes => {
+        const webamp = new Webamp({
+          zIndex: 1000,
+          initialTracks: episodes
+            .filter(({ file = {} }) => file.asset)
+            .map(({ title, file = {} }) => ({
+              metaData: {
+                artist: "Syntax.fm",
+                title
+              },
+              url: file.asset.url
+            })),
+          // Optional. The default skin is included in the js bundle, and will be loaded by default.
+          initialSkin: {
+            // Can be downloaded from https://github.com/captbaritone/webamp/raw/master/skins/TopazAmp1-2.wsz
+            url: "/static/base-2.91.wsz"
+          }
+        });
+        // Render after the skin has loaded.
+        webamp.renderWhenReady(document.getElementById("webamp-container"));
+        this.setState({ webamp });
+      });
+  };
+
+  componentWillUnmount() {
+    /* I have no idea what I'm doing */
+    this.state.webamp.store.dispatch({ type: "STOP" });
+    const webamp = document.getElementById("webamp");
+    const webampFileInput = document.getElementById("webamp-file-input");
+    ReactDOM.unmountComponentAtNode(webamp);
+    ReactDOM.unmountComponentAtNode(webampFileInput);
+    webamp.remove();
+    webampFileInput.remove();
+  }
+  render() {
+    return <div ref="webampContainer" id="webamp-container" />;
+  }
+}
